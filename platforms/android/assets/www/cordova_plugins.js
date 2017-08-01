f494c7cd7b782210600d55f6fa0200b7e48a0121
webmobilidev <webmobilisviluppo@MacBook-Air-di-Luca.local>
2017-08-01 11:01:13 +0200

    prepare android only

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,80 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "cordova-plugin-contacts.contacts",
-        "file": "plugins/cordova-plugin-contacts/www/contacts.js",
-        "pluginId": "cordova-plugin-contacts",
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
         "clobbers": [
-            "navigator.contacts"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.Contact",
-        "file": "plugins/cordova-plugin-contacts/www/Contact.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "Contact"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.convertUtils",
-        "file": "plugins/cordova-plugin-contacts/www/convertUtils.js",
-        "pluginId": "cordova-plugin-contacts"
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactAddress",
-        "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactAddress"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactError",
-        "file": "plugins/cordova-plugin-contacts/www/ContactError.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactError"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactField",
-        "file": "plugins/cordova-plugin-contacts/www/ContactField.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactField"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactFindOptions",
-        "file": "plugins/cordova-plugin-contacts/www/ContactFindOptions.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactFindOptions"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactName",
-        "file": "plugins/cordova-plugin-contacts/www/ContactName.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactName"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactOrganization",
-        "file": "plugins/cordova-plugin-contacts/www/ContactOrganization.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactOrganization"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactFieldType",
-        "file": "plugins/cordova-plugin-contacts/www/ContactFieldType.js",
-        "pluginId": "cordova-plugin-contacts",
-        "merges": [
-            ""
+            "PushNotification"
         ]
     },
     {
@@ -148,31 +79,21 @@ module.exports = [
         "clobbers": [
             "window.plugins.socialsharing"
         ]
-    },
-    {
-        "id": "phonegap-plugin-push.PushNotification",
-        "file": "plugins/phonegap-plugin-push/www/push.js",
-        "pluginId": "phonegap-plugin-push",
-        "clobbers": [
-            "PushNotification"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-custom-config": "3.0.14",
-    "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-contacts": "2.3.1",
+    "cordova-plugin-whitelist": "1.2.2",
+    "phonegap-plugin-push": "1.8.4",
     "cordova-plugin-device": "1.1.6",
+    "cordova-custom-config": "3.0.14",
     "cordova-plugin-dialogs": "1.2.1",
     "cordova-plugin-inappbrowser": "1.5.0",
     "cordova-plugin-network-information": "1.2.1",
     "cordova-plugin-statusbar": "2.2.3",
-    "cordova-plugin-whitelist": "1.2.2",
     "es6-promise-plugin": "4.1.0",
-    "cordova-plugin-x-socialsharing": "5.1.8",
-    "phonegap-plugin-push": "1.8.4"
+    "cordova-plugin-x-socialsharing": "5.1.8"
 };
 // BOTTOM OF METADATA
 });
